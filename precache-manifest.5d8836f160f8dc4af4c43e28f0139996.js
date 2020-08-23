530d187491e41eb0750ae996b154590a548b568b
afroz9008 <54906553+afroz9008@users.noreply.github.com>
2020-08-22 22:39:16 -0700

    Add files via upload

---
+++ precache-manifest.5d8836f160f8dc4af4c43e28f0139996.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe033e1ad30796ec7aef6667fdf2480",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec909d08b0ce01fd1052",
+    "url": "/static/css/main.f55ad622.chunk.css"
+  },
+  {
+    "revision": "c30e8f431f7a2e6a08d5",
+    "url": "/static/js/2.ce237462.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce237462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec909d08b0ce01fd1052",
+    "url": "/static/js/main.a3160e18.chunk.js"
+  },
+  {
+    "revision": "8d71aa98c6dda6a1aab8",
+    "url": "/static/js/runtime-main.f2f0ceec.js"
+  },
+  {
+    "revision": "a8ac942001c4b958bc55735d48d8b869",
+    "url": "/static/media/img.a8ac9420.jpg"
+  }
+]);
